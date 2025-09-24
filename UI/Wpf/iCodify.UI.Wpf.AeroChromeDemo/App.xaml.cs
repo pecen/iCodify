8f3d66a377c8f5f333f4babff0d1a33162672c62
DESKTOP-EM2EV5T\Peter <peter.centellini@redesajn.com>
2025-09-24 18:00:54 +0200

    Initial add of AeroChromeDemo project

---
+++ UI/Wpf/iCodify.UI.Wpf.AeroChromeDemo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace iCodify.UI.Wpf.AeroChromeDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
