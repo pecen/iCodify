b9cc5006819366030dced7ff17eef3974510723a
PETERCENTEL5929\peter <peter.centellini@magello.se>
2025-10-01 17:53:49 +0200

    Changed APIKey for Opper

--- src/Dal/iCodify.DalMock/MockDb/Database.cs
+++ src/Dal/iCodify.DalMock/MockDb/Database.cs
@@ -10,7 +10,8 @@ namespace iCodify.DalMock.MockDb
 		{
 			APIKeys = new List<APIKeyData>
 			{
-				new APIKeyData{ Id = 0, Model = APIModels.Opper, APIKey = "op-ESZCBUT1VRVO2F3TGTI8", Endpoint = "https://api.opper.ai/v2/call" },
+				//new APIKeyData{ Id = 0, Model = APIModels.Opper, APIKey = "op-ESZCBUT1VRVO2F3TGTI8", Endpoint = "https://api.opper.ai/v2/call" },
+				new APIKeyData{ Id = 0, Model = APIModels.Opper, APIKey = "op-A2EGIMSYY4RJILF8IM0X", Endpoint = "https://api.opper.ai/v2/call" },
 				new APIKeyData{ Id = 1, Model = APIModels.OpenAI, APIKey = "sadlofjewpfedhkgaldfv", Endpoint = "https://api.chatgpt.com/v2/request" },
 				new APIKeyData{ Id = 2, Model = APIModels.Gemini, APIKey = "ewroiyjbkldbqrrrö-4rkf", Endpoint = "https://api.gemini.ai/v2/getanswer" }
 			};
